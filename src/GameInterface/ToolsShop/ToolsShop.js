60fd673cad7751543a43eade2061eed5889aed47
adrianoli <adrianoliwa@o2.pl>
2022-06-12 18:49:38 +0200

    Plik konfiguracyjny plus tworzenie narzędzie dla sklepu

--- src/GameInterface/ToolsShop/ToolsShop.js
+++ src/GameInterface/ToolsShop/ToolsShop.js
@@ -1,10 +1,32 @@
 import React from "react";
 import styles from "./ToolsShop.module.css";
+import { Button, Tooltip } from "antd";
+import machinesData from "../../../public/configs/machines.json";
 
 const ToolsShop = () => {
+  console.log(machinesData[0]);
+
   return (
     <div className={styles.shopDiv}>
       <h1 className={styles.shopH1}>Sklep</h1>
+      <div className={styles.machineDiv}>
+        {machinesData.map((machine) => {
+          return (
+            <Button
+              key={machine.machineId}
+              icon={
+                <img
+                  className={styles.machineItemImg}
+                  src={machine.imagePath}
+                  alt={machine.machineName}
+                />
+              }
+              className={styles.machineItem}
+              type="primary"
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
